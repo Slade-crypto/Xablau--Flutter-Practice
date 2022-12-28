e167b77d9317cc6bcf7f705c3cf11fce5c32c456
Renan Feliciano <renanfrocha6@gmail.com>
2022-12-28 16:23:49 -0300

    personalização tela com mascara e imagem

--- lib/paginas/home_principal.dart
+++ lib/paginas/home_principal.dart
@@ -6,9 +6,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ambiente teste'),
-      ),
       body: Center(
         child: Stack(
           alignment: Alignment.center,
@@ -16,16 +13,16 @@ class HomePage extends StatelessWidget {
             SizedBox(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
-              child: Image.network(
-                'https://i.kym-cdn.com/entries/icons/mobile/000/025/067/ugandanknuck.jpg',
+              child: Image.asset(
+                "assets/imgs/background_uganda.png",
                 fit: BoxFit.cover,
               ),
             ),
             Container(
               alignment: Alignment.center,
-              width: 70,
-              height: 70,
-              color: Colors.blue,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.black.withOpacity(0.60),
               child: Text(
                 'Ei bixa',
                 style: TextStyle(
@@ -34,6 +31,23 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  const FlutterLogo(
+                    size: 150,
+                    style: FlutterLogoStyle.horizontal,
+                    textColor: Colors.white,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Entrar no app'),
+                  ),
+                ],
               ),
             ),
           ],
